714a0f624c2a470d6e431ef8499b127de02a8a31
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-08-22 19:14:58 +0300

    Create User model in src/database/models

--- src/database/models/accounts.py
+++ src/database/models/accounts.py
@@ -1,4 +1,18 @@
 import enum
+from datetime import datetime
+from typing import Optional, List
+
+from sqlalchemy import (
+    Integer,
+    String,
+    Boolean,
+    DateTime,
+    func,
+    ForeignKey
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.database.models.base import Base
 
 
 class UserGroupEnum(str, enum.Enum):
@@ -10,3 +24,43 @@ class UserGroupEnum(str, enum.Enum):
 class GenderEnum(str, enum.Enum):
     MAN = "man"
     WOMAN = "woman"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False, index=True)
+    _hashed_password: Mapped[str] = mapped_column("hashed_password", String(255), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False
+    )
+    group_id: Mapped[int] = mapped_column(ForeignKey("user_groups.id", ondelete="CASCADE"), nullable=False)
+
+    group: Mapped["UserGroup"] = relationship("UserGroup", back_populates="users")
+
+    profile: Mapped[Optional["UserProfileModel"]] = relationship(
+        "UserProfileModel",
+        back_populates="user",
+        cascade="all, delete-orphan"
+    )
+
+    activation_token: Mapped[Optional["ActivationTokenModel"]] = relationship(
+        "ActivationTokenModel",
+        back_populates="user",
+        cascade="all, delete-orphan"
+    )
+    password_reset_token: Mapped[Optional["PasswordResetTokenModel"]] = relationship(
+        "PasswordResetTokenModel",
+        back_populates="user",
+        cascade="all, delete-orphan"
+    )
+    refresh_tokens: Mapped[List["RefreshTokenModel"]] = relationship(
+        "RefreshTokenModel",
+        back_populates="user",
+        cascade="all, delete-orphan"
+    )
