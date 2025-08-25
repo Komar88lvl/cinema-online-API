5bc261305bcae694ef9efd9a229f2e4dd14a13fc
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-08-25 15:27:21 +0300

    Create TokenRefreshResponseSchema

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -54,3 +54,8 @@ class MessageResponseSchema(BaseModel):
 
 class TokenRefreshRequestSchema(BaseModel):
     refresh_token: str
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
