e660f631cf21345bd2ff97366e079040081835da
Sviatoslav Antonishyn <djsv91@gmail.com>
2025-09-20 11:25:36 +0300

    Save routers & main.py changes

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from src.routes import accounts_router
 
+app = FastAPI(
+    title="Movies homework",
+    description="Description of project"
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+api_version_prefix = "/api/v1"
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
